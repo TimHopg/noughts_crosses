a304a8ae17af68f3bdea751808cbfbdfca128cbb
TimHopg <timhopgood@gmail.com>
2021-02-09 00:30:05 +0000

    removed global variables made more efficient with great help from tom
    todo
    game not looping
    alternate starting player

--- game.py
+++ game.py
@@ -1,24 +1,36 @@
 from tabulate import tabulate as tab
 
-#
+# don't need all vars defined so early
 turn_count = 0
 game_count = 0
-play_mark = "X"
-player_input = ""
-next_player = ""
-win_name = ""
 input_variable = ""
 again = "y"
+winner = -1
+players_list = [1, 2]
 
 
 class Player:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
+    def __init__(self):
+        self.name = None
+        self.score = 0
+        self.symbol = None
+
+    def prompt_name(self, symbol):
+        self.name = input("Enter your name: ")
+        self.symbol = symbol
+
+        print("Welcome %s, you are %ss."
+              % (self.name, self.symbol))
 
 
-player_1 = Player("", 0)
-player_2 = Player("", 0)
+def switch_players(player, players):
+    if player not in players:
+        print("Something went wrong")
+    # following doesn't scale well
+    if current_player == players[0]:
+        return players[1]
+    else:
+        return players[0]
 
 
 # sets blank game grid
@@ -38,6 +50,7 @@ rows = [1, 2, 3]
 
 
 # functions to convert player input to int coordinates
+# can combine these into one function with methods
 def function_x(x):
     if x[0] == "a":
         return 0
@@ -62,29 +75,9 @@ def print_table():
 
 
 # turn function
-def turn():
-    # look to remove global vars
-    global next_player
-    global turn_count
-
+def turn(player):
     while True:
         try:
-            # simplify following
-            if (game_count % 2) == 0:
-                if (turn_count % 2) == 0:
-                    ox = "O"
-                    next_player = player_2.name
-                else:
-                    ox = "X"
-                    next_player = player_1.name
-            else:
-                if (turn_count % 2) == 0:
-                    ox = "X"
-                    next_player = player_1.name
-                else:
-                    ox = "O"
-                    next_player = player_2.name
-
             player_guess = input(input_variable)
 
             guess_x = function_x(player_guess)
@@ -94,7 +87,8 @@ def turn():
                 print("\nChoose empty field")
                 continue
             elif game_grid[guess_y][guess_x] == "":
-                game_grid[guess_y][guess_x] = ox
+                game_grid[guess_y][guess_x] = player.symbol
+                print_table()
                 break
         except TypeError:
             print("\nInvalid grid reference")
@@ -103,114 +97,76 @@ def turn():
         else:
             break
 
-    print_table()
-
-    turn_count += 1
-
 
 # win check function
 def win_check():
     for row in game_grid:  # check rows
         if row[0] == row[1] == row[2] and row[1] != "":
-            return True
+            return row[0]
     for x in range(3):  # check columns
         if game_grid[0][x] == game_grid[1][x] == game_grid[2][x] and game_grid[1][x] != "":
-            return True
+            return game_grid[0][x]
     if game_grid[0][0] == game_grid[1][1] == game_grid[2][2] and game_grid[0][0] != "":  # diagonal down
-        return True
+        return game_grid[0][0]
     elif game_grid[2][0] == game_grid[1][1] == game_grid[0][2] and game_grid[1][1] != "":  # diagonal up
-        return True
-
-
-def draw_check():
+        return game_grid[2][0]
     if turn_count == 9:
-        return True
+        return "draw"
+    return -1
 
 
-def print_winner():
-    global win_name
-    # also simplify the following
-    if (game_count % 2) == 0:
-        if (turn_count % 2) == 0:
-            win_name = player_2.name
-        else:
-            win_name = player_1.name
-    else:
-        if (turn_count % 2) == 0:
-            win_name = player_1.name
-        else:
-            win_name = player_2.name
-    print("Game over! " + win_name + " wins!")
-
-
-def score_display():
-    if win_name == player_1.name:
-        player_1.score += 1
-    elif win_name == player_2.name:
-        player_2.score += 1
+def score_display(player):
+    player.score += 1
     print("\nScore:\n" +
           str(player_1.name) + " = " + str(player_1.score) + "\n" +
-          str(player_2.name) + " = " + str(player_2.score)
-          )
+          str(player_2.name) + " = " + str(player_2.score))
 
+
+player_1 = Player()
+player_2 = Player()
 
 # game
+if __name__ == "__main__":
+    print('\n'"Welcome to Noughts and Crosses" '\n')
+    print("Player 1 ")
+    player_1.prompt_name("O")
+    print("\nPlayer 2 ")
+    player_2.prompt_name("X")
+    players_list = [player_1, player_2]
+    current_player = player_1
+    print_table()
+    print("\nEnter grid reference in the form of a letter then number e.g. b2.")
 
-print('\n'"Welcome to Noughts and Crosses" '\n')
+    while again == "y":
+        input_variable = "\n" + current_player.name + ", take your turn: "
+        turn(current_player)
+        turn_count += 1
+        winner = win_check()
+        if winner != (-1 or "draw"):
+            print("Game over! " + current_player.name + " wins!")
+            game_count += 1
+            turn_count = 0
+            score_display(current_player)
+            reset_grid(game_grid)
+            winner = -1
+            break
+        elif winner == "draw":
+            print("\nIt's a draw")
+            game_count += 1
+            turn_count = 0
+            score_display(current_player)
+            reset_grid(game_grid)
+            winner = -1
+            break
+        current_player = switch_players(current_player, players_list)
 
-print_table()
+    again = input("\nWould you like to play again, y/n? ")
 
-while True:
-    if turn_count == 0 and game_count == 0:
-        player_1.name = input("Player 1, enter your name: ")
-        next_player = player_1.name
-        input_variable = "\n%s, you are noughts.\n" \
-                         "Enter grid reference in the form of a letter then number e.g. """""b2" """ ": " \
-                         % next_player
-        turn()
-
-    elif turn_count == 1 and game_count == 0:
-        player_2.name = input("\nPlayer 2, enter your name: ")
-        next_player = player_2.name
-        input_variable = "\n%s, you are crosses.\n" \
-                         "Enter grid reference of empty field: " \
-                         % next_player
-        turn()
-
-    else:
-        while again == "y":
-            if turn_count > 1 or game_count >= 1:
-                input_variable = "\n" + next_player + ", take your turn: "
-                turn()
-                win_check()
-
-            if win_check():
-                pass
-            elif draw_check():
-                pass
-
-            if win_check():
-                print_winner()
-                game_count += 1
-                turn_count = 0
-                score_display()
-                reset_grid(game_grid)
-                break
-            elif draw_check():
-                print("\nIt's a draw")
-                game_count += 1
-                turn_count = 0
-                score_display()
-                reset_grid(game_grid)
-                break
-
-        again = input("\nWould you like to play again, y/n? ")
-
-        while again != ("y", "n"):
-            if again == "y":
-                print_table()
-                break
-            elif again == "n":
-                exit()
-            else:
-                break
+    while again != ("y", "n"):
+        if again == "y":
+            print_table()
+            break
+        elif again == "n":
+            exit()
+        else:
+            break
